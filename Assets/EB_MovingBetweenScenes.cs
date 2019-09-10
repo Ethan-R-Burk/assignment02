62ff041415e44c755b4edde56ebb60289a6ee8e5
Ethan-R-Burk <54331592+Ethan-R-Burk@users.noreply.github.com>
2019-09-10 09:50:49 -0500

    Add files via upload

---
+++ Assets/EB_MovingBetweenScenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EB_Scenes : MonoBehaviour
+{
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("Playing_EB");
+    }
+
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit_EB");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro_EB");
+    }
+
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# assignment02
+# Assignment02
+ 
